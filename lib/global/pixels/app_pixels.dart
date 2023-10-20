bb2c98988b0b7eb0257f5c61dbf76615fd091f89
Wasim <wasimxaman13@gmail.com>
2023-10-20 20:09:55 +0500

    update

--- lib/global/pixels/app_pixels.dart
+++ lib/global/pixels/app_pixels.dart
@@ -1,5 +1,5 @@
 abstract class AppPixels {
-  static const double heading = 28.0;
+  static const heading = 28.0;
   static const subHeading = 18.0;
   static const normal = 12.0;
   static const button = 18.0;

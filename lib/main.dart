387ccf029ca57068c78fb223e825b28d64636e40
Wasim <wasimxaman13@gmail.com>
2023-10-24 14:41:50 +0500

    reusable app bar used in the notification screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hobbyzhub/constants/bloc_provider.dart';
 import 'package:hobbyzhub/global/themes/app_theme.dart';
-import 'package:hobbyzhub/views/auth/complete_profile_screen1.dart';
-import 'package:hobbyzhub/views/bottom_nav_bar/main_tabs_screen.dart';
+import 'package:hobbyzhub/views/notification/notification_screen.dart';
 import 'package:nb_utils/nb_utils.dart';
 
 void main() async {
@@ -39,9 +38,7 @@ class _MyAppState extends State<MyApp> {
                 title: 'HobbyzHub',
                 theme: AppTheme.light,
                 navigatorKey: navigatorKey,
-                home: MainTabScreen(
-                  index: 0,
-                ),
+                home: NotificationScreen(),
               ),
             );
           }),

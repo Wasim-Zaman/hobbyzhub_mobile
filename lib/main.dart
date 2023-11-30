46b29d49e8dafad17274ea4036ac9d5151cc18cd
bakht biland <62112170+bakht200@users.noreply.github.com>
2023-11-30 23:55:30 +0500

    add some packages

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,16 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hobbyzhub/constants/bloc_provider.dart';
 import 'package:hobbyzhub/global/themes/app_theme.dart';
+import 'package:hobbyzhub/views/bottom_nav_bar/main_tabs_screen.dart';
 import 'package:hobbyzhub/views/splash_screen/splash_screen.dart';
+import 'package:hobbyzhub/views/welcome/welcome_screen.dart';
 import 'package:nb_utils/nb_utils.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initialize();
+  timeago.setLocaleMessages('fr', timeago.EnMessages());
   runApp(const MyApp());
 }
 
@@ -38,7 +42,9 @@ class _MyAppState extends State<MyApp> {
                 title: 'HobbyzHub',
                 theme: AppTheme.light,
                 navigatorKey: navigatorKey,
-                home: const SplashScreen(),
+                home: MainTabScreen(
+                  index: 0,
+                ),
               ),
             );
           }),

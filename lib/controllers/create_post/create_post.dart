1cf1bb8b26d86a1aafe11ef8f0e4c6c599cdae2d
bakht biland <62112170+bakht200@users.noreply.github.com>
2023-12-01 15:07:38 +0500

    implementation of create post

--- lib/controllers/create_post/create_post.dart
+++ lib/controllers/create_post/create_post.dart
@@ -3,38 +3,57 @@ import 'dart:io';
 
 import 'package:hobbyzhub/constants/app_url.dart';
 import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
 
 class PostController {
   createPost(List<File> files) async {
-    var request = http.MultipartRequest('POST', Uri.parse(PostUrl.createPost));
+    // Replace the URL with your server endpoint
+    final url = Uri.parse(PostUrl.createPost);
+
+    // Create a new multipart request
+    final request = http.MultipartRequest('POST', url);
 
     // Add files to the request
-    for (var file in files) {
-      var length = await file.length();
-      var multipartFile = http.MultipartFile('files', file.openRead(), length,
-          filename: file.path.split('/').last);
+    List<String> filePaths = [];
 
-      request.files.add(multipartFile);
+    for (var i = 0; i < files.length; i++) {
+      filePaths.add(files[i].path);
+    }
+    for (String filePath in filePaths) {
+      String fileName = filePath.split('/').last;
+      request.files.add(
+        await http.MultipartFile.fromPath(
+          'files',
+          filePath,
+          filename: fileName,
+        ),
+      );
     }
 
-    // Add text in string to the request
-    request.fields['userId'] = "hashdhad";
+    // Add query parameters
+    request.fields['userId'] = 'hasdasdha';
 
-    // Add text in map to the request
-    // request.fields['createPost'] = jsonEncode("First her");
-    // print(request.fields['createPost']);
+    // Add JSON body
+    Map<String, dynamic> jsonBody = {'caption': 'hello'};
+    request.files.add(
+      http.MultipartFile.fromString(
+        'createPost',
+        json.encode(jsonBody),
+        contentType: MediaType('application', 'json'),
+      ),
+    );
 
     // Send the request
     try {
-      var response = await request.send();
-      if (response.statusCode == 200) {
-        print('Multipart request sent successfully');
+      final response = await request.send();
+
+      if (response.statusCode == 201) {
+        print('Request successful');
       } else {
-        print(
-            'Failed to send multipart request. Status code: ${response.statusCode}');
+        print('Request failed with status: ${response.statusCode}');
       }
     } catch (error) {
-      print('Error sending multipart request: $error');
+      print('Error sending request: $error');
     }
   }
 }

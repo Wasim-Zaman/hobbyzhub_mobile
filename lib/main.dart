d1547cc8a3d4a3324ff47f7011ee656df056af85
Wasim <wasimxaman13@gmail.com>
2023-12-14 09:36:40 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hobbyzhub/constants/bloc_provider.dart';
 import 'package:hobbyzhub/global/themes/app_theme.dart';
-import 'package:hobbyzhub/views/bottom_nav_bar/main_tabs_screen.dart';
 import 'package:hobbyzhub/views/splash_screen/splash_screen.dart';
-import 'package:hobbyzhub/views/welcome/welcome_screen.dart';
 import 'package:nb_utils/nb_utils.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
@@ -38,11 +36,12 @@ class _MyAppState extends State<MyApp> {
                 hideKeyboard(context);
               },
               child: MaterialApp(
-                  debugShowCheckedModeBanner: false,
-                  title: 'HobbyzHub',
-                  theme: AppTheme.light,
-                  navigatorKey: navigatorKey,
-                  home: SplashScreen()),
+                debugShowCheckedModeBanner: false,
+                title: 'HobbyzHub',
+                theme: AppTheme.light,
+                navigatorKey: navigatorKey,
+                home: const SplashScreen(),
+              ),
             );
           }),
     );

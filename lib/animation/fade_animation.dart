49f1b336e13bd5d4c0afcd9c9cb309343544306c
Wasim <wasimxaman13@gmail.com>
2024-01-03 09:57:57 +0500

    updated animation

--- lib/animation/fade_animation.dart
+++ lib/animation/fade_animation.dart
@@ -2,21 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:simple_animations/simple_animations.dart';
 import 'package:supercharged/supercharged.dart';
 
-enum AniProps { opacity, translateY }
+enum AniProps { opacity, translateX }
+
+enum MotionDirection { leftToRight, rightToLeft }
 
 class FadeAnimation extends StatelessWidget {
   final double delay;
   final Widget child;
+  final MotionDirection direction;
 
-  const FadeAnimation({super.key, required this.delay, required this.child});
+  const FadeAnimation({
+    Key? key,
+    required this.delay,
+    required this.child,
+    this.direction = MotionDirection.leftToRight,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final tween = MultiTween<AniProps>()
       ..add(AniProps.opacity, 0.0.tweenTo(1.0), 500.milliseconds)
       ..add(
-        AniProps.translateY,
-        (-30.0).tweenTo(0.0),
+        AniProps.translateX,
+        (direction == MotionDirection.leftToRight ? 30.0 : -30.0).tweenTo(0.0),
         500.milliseconds,
         Curves.easeOut,
       );
@@ -26,11 +34,15 @@ class FadeAnimation extends StatelessWidget {
       duration: tween.duration,
       tween: tween,
       child: child,
-      builder: (context, child, value) => Opacity(
-        opacity: value.get(AniProps.opacity),
-        child: Transform.translate(
-            offset: Offset(0, value.get(AniProps.translateY)), child: child),
-      ),
+      builder: (context, child, value) {
+        return Opacity(
+          opacity: value.get(AniProps.opacity),
+          child: Transform.translate(
+            offset: Offset(value.get(AniProps.translateX), 0),
+            child: child,
+          ),
+        );
+      },
     );
   }
 }

78a9fcd9549e9c39a1888f4a89a7bfdb453216cb
Wasim <wasimxaman13@gmail.com>
2024-01-03 19:04:30 +0500

    updated

--- lib/views/profile/third_person_profile_screen.dart
+++ lib/views/profile/third_person_profile_screen.dart
@@ -314,28 +314,3 @@ class NonFollowedPersonWidget extends StatelessWidget {
     );
   }
 }
-
-class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
-  _SliverAppBarDelegate(this._tabBar);
-
-  final TabBar _tabBar;
-
-  @override
-  double get minExtent => _tabBar.preferredSize.height;
-  @override
-  double get maxExtent => _tabBar.preferredSize.height;
-
-  @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Container(
-      color: Colors.white,
-      child: _tabBar,
-    );
-  }
-
-  @override
-  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
-    return false;
-  }
-}

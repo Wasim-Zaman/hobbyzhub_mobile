10885139243d8bda49b0656492f666bab63c70f8
Wasim <wasimxaman13@gmail.com>
2024-01-08 12:00:59 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class _MyAppState extends State<MyApp> {
               title: 'HobbyzHub',
               theme: AppTheme.light,
               navigatorKey: navigatorKey,
-              home: SplashScreen(),
+              home: const SplashScreen(),
             ),
           );
         },

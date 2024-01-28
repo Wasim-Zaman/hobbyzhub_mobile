b926aa107b723b798e94a5ad24a2b0dd3229383e
Wasim <wasimxaman13@gmail.com>
2024-01-28 13:32:54 +0500

    chat feature added

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hobbyzhub/constants/bloc_provider.dart';
 import 'package:hobbyzhub/global/themes/app_theme.dart';
-import 'package:hobbyzhub/views/messaging/messaging_screen.dart';
+import 'package:hobbyzhub/views/splash_screen/splash_screen.dart';
 import 'package:nb_utils/nb_utils.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
@@ -41,8 +41,7 @@ class _MyAppState extends State<MyApp> {
               title: 'HobbyzHub',
               theme: AppTheme.light,
               navigatorKey: navigatorKey,
-              home: const MessagingScreen(userId: "ws1234"),
-              // home: const ChatScreen(),
+              home: const SplashScreen(),
             ),
           );
         },

459763525fcdd0938d03959daee842d9e8720d78
bakht biland <62112170+bakht200@users.noreply.github.com>
2024-03-01 23:54:01 +0500

    update the setting screen

--- lib/constants/bloc_provider.dart
+++ lib/constants/bloc_provider.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hobbyzhub/blocs/auth/auth_bloc.dart';
 import 'package:hobbyzhub/blocs/chat/chat_bloc.dart';
-import 'package:hobbyzhub/blocs/create_conversation/create_conversation_cubit.dart';
 import 'package:hobbyzhub/blocs/create_post/createpost_cubit.dart';
 import 'package:hobbyzhub/blocs/create_story/create_story_cubit.dart';
 import 'package:hobbyzhub/blocs/delete_fcm_token/delete_fcm_token_cubit.dart';
@@ -10,7 +9,6 @@ import 'package:hobbyzhub/blocs/fcm_token/fcm_token_cubit.dart';
 import 'package:hobbyzhub/blocs/follower_following/f_and_f_bloc.dart';
 import 'package:hobbyzhub/blocs/get_post/get_post_cubit.dart';
 import 'package:hobbyzhub/blocs/get_stories/get_stories_cubit.dart';
-import 'package:hobbyzhub/blocs/get_user/get_users_cubit.dart';
 import 'package:hobbyzhub/blocs/group/group_bloc.dart';
 import 'package:hobbyzhub/blocs/help_center/help_center_cubit.dart';
 import 'package:hobbyzhub/blocs/image_picker/image_picker_bloc.dart';
@@ -48,11 +46,8 @@ class BlocProviders {
     BlocProvider<UpdateProfileCubit>(create: (context) => UpdateProfileCubit()),
     BlocProvider<FcmTokenCubit>(create: (context) => FcmTokenCubit()),
     BlocProvider<UnlikePostCubit>(create: (context) => UnlikePostCubit()),
-    BlocProvider<GetUsersCubit>(create: (context) => GetUsersCubit()),
     BlocProvider<CreateStoryCubit>(create: (context) => CreateStoryCubit()),
     BlocProvider<GetStoriesCubit>(create: (context) => GetStoriesCubit()),
-    BlocProvider<CreateConversationCubit>(
-        create: (context) => CreateConversationCubit()),
     BlocProvider<DeleteFcmTokenCubit>(
         create: (context) => DeleteFcmTokenCubit()),
     BlocProvider<GroupBloc>(create: (context) => GroupBloc()),
